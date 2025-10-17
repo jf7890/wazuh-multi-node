0e76156596a03ae7c141e622848c46ee8a0ef2c2
jf7890 <nguyenvannhan601@gmail.com>
2025-10-17 10:41:55 +0700

    ok

--- certs.sh
+++ certs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 docker run --rm -ti \
-   -v /config/certs:/config \
-   -v /config/certs:/certificates \
-   wazuh/wazuh-certs-generator:0.0.2
+  -v $(pwd)/config:/config \
+  -v $(pwd)/config/certs:/certificates \
+  wazuh/wazuh-certs-generator:0.0.2
 
 chmod -R 755 config/certs
 chmod -R 666 config/certs/*.pem
